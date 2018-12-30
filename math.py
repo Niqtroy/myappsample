69ae412edbaf8ede743bf15e68c9b3a2dd524c53
troyent <troyent.niq@gmail.com>
2018-12-30 14:39:07 +0200

    math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+x = 1
+if x > 3 :
+    print(" x is greater thsn3")
+    if > 2:
+        print("x is greater 2")
+        if x = 3:
+            print("x is equal to 3")
+else:
+    print("x is less than 3")
+else:
+    print("x is is less than 2")
+else:
+    print("x is not equ to 3")
